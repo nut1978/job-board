f107356548ec0b05d8e9ddfe92c0415fa602b44b
Nuttaporn Thummachat <nuttaporn.thummachat@bnc.ca>
2021-10-14 15:59:10 +0700

    training

--- server/resolves.js
+++ server/resolves.js
@@ -7,6 +7,13 @@ const Query = {
     jobs: () => db.jobs.list()
 };
 
+const Mutation = {
+    createJob: (root, {input}) => {
+        const id = db.jobs.create(input);
+        return db.jobs.get(id);
+    }
+};
+
 const Company = {
     jobs: (company) => db.jobs.list()
         .filter((job) => job.companyId === company.id)
@@ -16,4 +23,4 @@ const Job = {
     company: (job) => db.companies.get(job.companyId)
 }
 
-module.exports = { Query, Company, Job };
+module.exports = { Query, Mutation, Company, Job };
