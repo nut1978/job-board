f7af44b3d3484676c5f19dca4e032648a1aacea6
Nuttaporn Thummachat <nuttaporn.thummachat@bnc.ca>
2021-10-14 14:18:54 +0700

    training

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,5 +1,29 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
+export async function loadJob(id) {
+    const response = await fetch(endpointURL, {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify({
+            query: `query JobQuery($id: ID!){
+                job(id: $id){
+                  id
+                  title
+                  description
+                  company {
+                    id
+                    name
+                  }
+                }
+              }`,
+              variables: {id}
+        })
+    });
+
+    const responseBody = await response.json();
+    return responseBody.data.job;
+}
+
 export async function loadJobs() {
     const response = await fetch(endpointURL, {
         method: 'POST',
@@ -7,10 +31,10 @@ export async function loadJobs() {
         body: JSON.stringify({
             query: `{
                 jobs {
-                  id,
-                  title,
+                  id
+                  title
                   company {
-                    id,
+                    id
                     name
                   }
                 }
